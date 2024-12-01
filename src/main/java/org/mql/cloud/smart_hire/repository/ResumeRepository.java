de0256e8a03e026f1d374878cc2248b052a18278
laktam <laktamlaktamabdellah@gmail.com>
2024-12-01 11:57:27 +0100

    add Resume repository

---
+++ src/main/java/org/mql/cloud/smart_hire/repository/ResumeRepository.java
@@ -0,0 +1,16 @@
+package org.mql.cloud.smart_hire.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import org.mql.cloud.smart_hire.model.Resume;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+public interface ResumeRepository extends MongoRepository<Resume, String>{
+	List<Resume> findByPost(String post);
+	@Query(value = "{}", fields = "{'_id' : 0, 'post' : 1}")
+    List<Resume> findDistinctPosts();
+}
